a3a15496e52340bae824cdacb2463bb7cb81d9e8
EverestOw <54610621+EverestOw@users.noreply.github.com>
2021-09-24 18:01:12 -0700

    Update server.py comments
    Tidy up docstrings.
    Clean up indentations.
    Add spacings between lines.
    Add type hints.
    Add implementation thoughts.
    Split long single-lines over 90 chars in length into multi-lines.

--- server.py
+++ server.py
@@ -3,50 +3,49 @@ import PodSixNet.Server
 from time import sleep
 from time import time
 class ClientChannel(PodSixNet.Channel.Channel):
-  '''client channel class docstring to do'''
-    def Network(self, data: dict):
+    '''client channel class docstring to do'''
+
+    def Network(self, data: dict):  # Function name isn't very descriptive!
         '''Console log. Prints data dictionary'''
+
         print(data)
+
     def Network_readyStatus(self, data: dict):
-        '''
-            Feeds data into PodSixNet server object?
-        '''
+        '''Feeds data into PodSixNet server object?'''
+
         self._server.readyStatus(data)
+
     def Network_recieveCommand(self, data: dict):
-        '''
-            Pushes data into PodSixNet server objects
-        '''
+        '''Pushes data into PodSixNet server objects'''
+
         self._server.start_time = time()    #    testing latency
         self._server.recieveCommand(data)
 
-    #ping stuff
+    ####    ping stuff  ####
     # def __init__(self, *args, **kwargs):
         # PodSixNet.Channel.__init__(self, *args, **kwargs)
         # self.count = 0
         # self.times = []
 
-    def Network_ping(self, data: dict):
-      '''
-        Pings the player and prints out the result on the server.
-      '''
+    def Network_ping(self, data: dict) -> None:
+        '''Pings the player and prints out the result on the server.'''
+
         print(self, "ping %d round trip time was %f" % (data["count"], time() - self.times[data["count"]]))
-        if self.count == 10:    #   While loop.
+        if self.count == 10:    #   Replace this with a while loop
             self.count = 0
             self.times = []
         else:
             self.Ping()
     
-    def Ping(self):
-        '''
-            Pings itself. Sends a message dictionary?
-        '''
+    def Ping(self) -> None:
+        '''Pings itself. Sends a message dictionary?'''
+
         # print(self, "Ping:", self.count)
         self.times.append(time())
-        self.Send({"action": "ping", "count": self.count})  # 
+        self.Send({"action": "ping", "count": self.count})
         self.count += 1 #   Move this counter to Network_ping
-    #end ping stuff
+    ####    end ping stuff  ####
     
-
     # def Network_moveUnitTile(self, data):
     #     self._server.moveUnitTile(data)
     # def Network_nextPhase(self, data):
@@ -71,11 +70,12 @@ class ClientChannel(PodSixNet.Channel.Channel):
      
     #     #tells server to place line
     #     self._server.placeLine(hv, x, y, data, self.gameid, num)
+
     def Close(self):
-        '''
-        Closes the server (which then closes the clients).
-        '''
+        '''Closes the server (which then closes the clients).'''
+
         self._server.close(self.gameid)
+
 class BoxesServer(PodSixNet.Server.Server):
  
     channelClass = ClientChannel
@@ -90,7 +90,9 @@ class BoxesServer(PodSixNet.Server.Server):
         Matches players with each other as they connect.
         Creates Game objects as needed.
         '''
+
         print('new connection:', channel)
+
         if self.current_game==None:
             self.currentIndex+=1
             channel.gameid=self.currentIndex
@@ -109,21 +111,23 @@ class BoxesServer(PodSixNet.Server.Server):
         self.start_time = 0
         # channel.Ping() #ping line
 
-    def readyStatus(self, data):    # Redundant? Message commands already
+    def readyStatus(self, data: dict):    # Redundant? Message commands already
                                     # Message commands already exist in Game
-      '''
-      Passes on message to game instance.
-      '''
+        '''
+        Passes on message to game instance.
+        '''
         game = [a for a in self.games if a.gameid==data["gameid"]]
         if len(game)==1:
             game[0].readyStatus(data)
 
-    def recieveCommand(self, data): # Redundant? Message commands already
+    def recieveCommand(self, data: dict): # Redundant? Message commands already
                                     # Message commands already exist in Game
-      '''
-      Passes on message to game instance.
-      '''
+        '''
+        Passes on message to game instance.
+        '''
+        # why use set building notation?
         game = [a for a in self.games if a.gameid==data["gameid"]]
+
         if len(game)==1:
             game[0].recieveCommand(data)
 
@@ -143,17 +147,21 @@ class BoxesServer(PodSixNet.Server.Server):
     #     game = [a for a in self.games if a.gameid==gameid]
     #     if len(game)==1:
     #         game[0].placeLine(is_h, x, y, data, num)
+
     def close(self, gameid):
-      '''
+        '''
         Closes the game instance and sends message to each player to close.
-      '''
+        '''
         try:
             game = [a for a in self.games if a.gameid==gameid][0]
             game.player0.Send({"action":"close"})
             game.player1.Send({"action":"close"})
-        except:
+        except: #   Should we be letting things silently fail?
+                #   Investigate what exceptions are thrown.
             pass
+
     def tick(self):
+        '''Pumps the game. Wrapper function around Channel.pump()'''
         # Check for any wins
         # Loop through all of the squares
         
@@ -180,35 +188,48 @@ class BoxesServer(PodSixNet.Server.Server):
         #     game.player0.Send({"action":"yourturn", "torf":True if self.games[index].turn==0 else False})
         #     index+=1
         self.Pump()
+
 class Game:
     def __init__(self, player0, currentIndex):
         # whose turn (1 or 0)
-        self.turn = 0   # 0 and 1 is too ambiguous. Set the value to an enumerated value.
+        self.turn = 0   # Refers to player0 or player1.
+                        # 0 and 1 is too ambiguous.
+                        # Set the value to an enumerated value.
+
         #owner map
-        self.owner=[[False for x in range(6)] for y in range(6)]
+        self.owner: list = [[False for x in range(6)] for y in range(6)]
+            #   Why are we making a 6x6 matrix full of False values?
+
         #initialize the players including the one who started the game
-        self.player0=player0
-        self.player1=None
+        self.player0 = player0
+        self.player1 = None
+        self.gameid = currentIndex
         #gameid of game
-        self.gameid=currentIndex
-    def readyStatus(self, data):
-      '''
-      Sends ready status to each player and starts the game.
-      '''
+
+    def readyStatus(self, data: dict):
+        '''Sends ready status to each player and starts the game.'''
+
         self.player0.Send({"action": "readyStatus", "player":data["player"]})
         self.player1.Send({"action": "readyStatus", "player":data["player"]})
 
-    def recieveCommand(self, data):
+    def recieveCommand(self, data: dict): # Refactor
         '''
-            [WIP]
-            Matches the {msg,value} to its corresponding action
-            Basically a switch state.
-            
-            Redundant function? Yes, but we'll use this system.
+        [WIP]
+        Sends Player 0 and Player 1 data from the server.
 
-            self.player0.Send(data)
-            self.player1.Send(data)
+        Matches the {msg: opcode} to its corresponding action.
+        Basically a switch state.
+        
+        Redundant function? Yes, but we'll use this system.
         '''
+        #   Maybe change this if-elif-chain into a dictionary.
+        #   Not sure how Python optimizes if-elif chains,
+        #   but we can use the hash-table nature of dictionaries
+        #   for O(1) performance.
+
+        #   The {key: value} would be (enumIndex, functionAction() )?
+        #   Is the overhead of calling another function too high?
+        #   Need to find a way to benchmark times
         if data["msg"] in [0]:
             self.player0.Send({"action": "recieveCommand", "msg":data["msg"], "state":data["state"], "player":data["player"]})
             self.player1.Send({"action": "recieveCommand", "msg":data["msg"], "state":data["state"], "player":data["player"]})
@@ -233,9 +254,6 @@ class Game:
         elif data["msg"] in [12]:
             self.player0.Send({"action": "recieveCommand", "msg":data["msg"], "boardid":data["boardid"]})
             self.player1.Send({"action": "recieveCommand", "msg":data["msg"], "boardid":data["boardid"]})
-
-
-
 
         elif data["msg"] in [25]:
             self.player0.Ping() #ping line
@@ -275,7 +293,9 @@ def start_server() -> None:
     '''
     Starts server.
     '''
-    address="localhost:8004"#"10.0.1.52:8002" this should be my local ip address anyway with the same port of 8002 but the server errors when started with the ip instad of localhost
+    address="localhost:8004"    # "10.0.1.52:8002" this should be my local ip address
+                                # anyway with the same port of 8002 but the server errors
+                                # when started with the ip instad of localhost
     print("STARTING SERVER ON " + address)
     if not address:
         host, port="localhost", 8004
@@ -287,4 +307,4 @@ def start_server() -> None:
         sleep(0.01)
 
 if __name__ == '__main__':
-    start_sever()
+    start_server()
